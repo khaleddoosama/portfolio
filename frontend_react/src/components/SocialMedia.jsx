312d38f910e8b2a73b2be05ec37f974a8e873cb2
khaleddoosama <khaleddoosama@gmail.com>
2023-09-19 04:52:38 +0300

    Code forces icon

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { BsTwitter, BsInstagram, BsLinkedin, BsGithub } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
-
+// code forces
+import { SiCodeforces } from 'react-icons/si';
 
 const SocialMedia = () => {
       return (
             <div className='app__social'>
                   <div>
-                        <a href="https://github.com/khaleddoosama" target="_blank">
+                        <a href="https://github.com/khaleddoosama" target="_blank" rel="noreferrer">
                               <BsGithub />
                         </a>
                   </div>
                   <div>
-                        <a href="https://www.linkedin.com/in/khaleddoosama/" target="_blank">
+                        <a href="https://www.linkedin.com/in/khaleddoosama/" target="_blank" rel="noreferrer">
                               <BsLinkedin />
                         </a>
                   </div>
                   <div>
-                        <a href="https://www.facebook.com/khaleddoosama" target="_blank">
-                              <FaFacebookF />
+                        <a href="https://codeforces.com/profile/khaleddoosama" target="_blank" rel="noreferrer">
+                              <SiCodeforces />
                         </a>
                   </div>
             </div>
